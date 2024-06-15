e4f2b66adeb4c1fb0ffcba8688b2453f76fc8f54
Jhonatan Rivera <jhontanjuarez723@gmail.com>
2024-06-14 23:10:58 -0500

    Actividad: Asignar evento de teclado en línea. Finalizada

---
+++ store/store/js/filterProducts.js
@@ -0,0 +1,8 @@
+// Define la función captureText para que imprima por consola “capturado”. 
+const captureText = () =>{
+    console.log(`capturado`);
+}
+
+// Identifica el input de búsqueda de index.html
+// Agrega el atributo onkeyup para asignar un evento de tipo keyup.
+/* <input type="text" placeholder="Search" onkeyup="captureText()" /> */
