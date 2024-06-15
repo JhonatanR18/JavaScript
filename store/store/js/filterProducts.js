a7fd2178d49eae27dfc7ca35b62d79bdb3a8885f
Jhonatan Rivera <jhontanjuarez723@gmail.com>
2024-06-15 12:13:06 -0500

    Actividad: Investigación del evento de teclado. Finalizada

--- store/store/js/filterProducts.js
+++ store/store/js/filterProducts.js
@@ -1,10 +1,12 @@
-// Define la función captureText para que imprima por consola “capturado”. 
-const captureText = () =>{
-    console.log("capturado");
+// modifica la función para que reciba un evento
+const captureText = (event) =>{
+    // imprime el evento completo
+    console.log(event);
+    // imprime el valor actual del campo de entrada
+    console.log(`Capturado: ${event.target.value}`);
 }
 
 // define una variable para seleccionar con el método querySelector
 let searchSelector = document.querySelector("#search");
-// agregar evento addEventListener 
-searchSelector.addEventListener("keyup", captureText)
-// evento tipo keyup - cada tecla presionada ejecuta la función captureText
+// modifica el escuchador | ahora depende de un objeto con todos los datos del evento
+searchSelector.addEventListener("keyup", event=>captureText(event))
