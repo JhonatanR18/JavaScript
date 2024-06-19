7688b30c091baf51779c40538a9a0f54099893dc
Jhonatan Rivera <jhontanjuarez723@gmail.com>
2024-06-19 14:08:20 -0500

    Actividad: Agregar un producto al carrito. Finalizada

--- store/store/js/productDetail.js
+++ store/store/js/productDetail.js
@@ -4,17 +4,20 @@ const query = location.search;
 // URLSearchParams define métodos útiles para trabajar con la cadena de consulta de una URL.
 const params = new URLSearchParams(query);
 const id = params.get('id')
-
+// formatear precios
+function formatPrice(precio){
+    return `S/ ${precio.toLocaleString('es-PE',{
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    })}`
+}
 // define una función que dependa del parámetro id del detalle del producto
 function printDetails(id){
     // guarde en una variable el producto encontrado con el id del parámetro
     const product = products.find(each => each.id === id)
     // Formatear el precio
-    const precioFormateado = `S/ ${product.priceWithDiscount.toLocaleString(
-        'es-PE', {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
-        })}`;
+    const priceDiscount = formatPrice(product.priceWithDiscount)
+    const priceNormal = formatPrice(product.normalPrice)
     // variable para la plantilla de la vista de detalles || con ${} pon los datos correspondientes al detalle del producto
     const detailsTemplate = `
     <div class="product-images-block">
@@ -46,8 +49,11 @@ function printDetails(id){
     </div>
     <div class="product-checkout-block">
         <div class="checkout-container">
-            <span class="checkout-total-label">Total:</span>
-            <h2 class="checkout-total-price">${precioFormateado}</h2>
+            <span class="checkout-total-label">Precio Exclusivo Web</span>
+            <div class="checkout-price-block">
+                <span id="price-nor" class="price-normal" data-value="${product.normalPrice}">${priceNormal}</span>
+                <h2 id="price-des" class="checkout-total-price" data-value="${product.priceWithDiscount}" >${priceDiscount}</h2>
+            </div>
             <p class="checkout-description">Incluye impuesto PAÍS y percepción AFIP. Podes recuperar AR$ 50711 haciendo la solicitud en AFIP.</p>
             <ul class="checkout-policy-list">
                 <li>
@@ -61,11 +67,11 @@ function printDetails(id){
             </ul>
             <div class="checkout-process">
                 <div class="top">
-                    <input type="number" value="1">
+                    <input id="quantity-1" type="number" value="1" onchange="subTotal(event)">
                     <button class="btn-primary">Comprar</button>
                 </div>
                 <div class="bottom">
-                    <button class="btn-outline">Añadir al Carrito</button>
+                    <button class="btn-outline" onclick="saveProduct(${product.id})">Añadir al Carrito</button>
                 </div>
             </div>
         </div>  
@@ -75,11 +81,52 @@ function printDetails(id){
     const detailsSelector = document.querySelector("#productDetail")
     detailsSelector.innerHTML = detailsTemplate
 }
+printDetails(id)
 // función click para cambiar de imagen
 function changeMini(event){
     const selectedSrc = event.target.src
     const bigSelector = document.querySelector("#big-img")
     bigSelector.src = selectedSrc
 }
+// función para cambiar precio // viene del atributo onchange en el input
+function subTotal(event){
+    // traer la cantidad del input en tipo number
+    const cantidadDefinida = event.target.value;
+    // traer el producto
+    const product = products.find(each => each.id == id)
+    // traer el selector de precio
+    const priceNormalSelector = document.querySelector("#price-nor")
+    // multiplicar la cantidad de productos por el precio
+    const subTotalNor = cantidadDefinida * product.normalPrice
+    priceNormalSelector.innerHTML = formatPrice(subTotalNor)
 
-printDetails(id)
+    // traer el selector de precio
+    const priceSelector = document.querySelector("#price-des")
+    // multiplicar la cantidad de productos por el precio 
+    const subTotalDes = cantidadDefinida * product.priceWithDiscount
+    // cambiar el precio total y formateo
+    priceSelector.innerHTML = formatPrice(subTotalDes)
+}
+// función click "añadir carrito" localStorage // viene del atributo onclick en el button añadir carrito
+// función que dependa del id del botón
+function saveProduct(id){
+    // busque el producto con el id
+    const product = products.find(each => each.id == id)
+    // calcular el precio total de acuerdo a la cantidad insertada en el input
+    const quantity = document.querySelector("#quantity-1").value
+    const subTotal = quantity * product.priceWithDiscount;
+    // objeto con las propiedades especificas de la compra
+    const objectProduct = {
+        id: product.id,
+        title: product.title,
+        photo: product.photo[0],
+        price: product.priceWithDiscount,
+        subTotal: subTotal,
+        colors: document.querySelector("#color").value,
+        quantity: document.querySelector("#quantity-1").value,
+    };
+    // convertir a JSON
+    const stringifyProduct = JSON.stringify(objectProduct )
+    // guardar el objeto en la memoria del navegador
+    localStorage.setItem('cart', stringifyProduct)
+    }
